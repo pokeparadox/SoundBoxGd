213c13d090f86e00fd937da752f6bd3944ca68b2
PokeParadox <pokeparadox@gmail.com>
2023-08-01 19:42:33 +0100

    Initial WIP commit Buttons are loaded from user folder

---
+++ ButtonsPanel.gd
@@ -0,0 +1,63 @@
+extends HFlowContainer
+
+const BUTTONS_PATH : String = "user://buttons"
+const WAV : String = "wav"
+const OGG : String = "ogg"
+const MP3 : String = "mp3"
+const PNG : String = "png"
+var SoundButton := load("res://Components/audio_button.tscn")
+
+enum SearchType
+{
+	DIR,
+	FILE,
+	ALL
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var button_dirs = _get_folder_names(BUTTONS_PATH, SearchType.DIR)
+	for bd in button_dirs:
+		var button_files = _get_folder_names(BUTTONS_PATH + "/" + bd, SearchType.FILE)
+		var has_sound : bool = false
+		var sound_path : String
+		var has_image : bool = false
+		var image_path : String
+		var button_name : String
+		for b in button_files:
+			var ext : String = b.get_extension()
+			var path : String = BUTTONS_PATH + "/" + bd + "/" + b
+			if ext == WAV or ext == OGG or ext == MP3:
+				has_sound = true
+				sound_path = path
+				button_name = bd.capitalize()
+			elif ext == PNG:
+				has_image = true
+				image_path = path
+		var button
+		if has_sound:
+			button = SoundButton.instantiate()
+			button.text = button_name
+			button.load_sound(sound_path)
+			self.add_child(button)
+		if has_image and button != null:
+			button.load_icon(image_path)
+			
+
+func _get_folder_names(root_folder : String, search_type : SearchType = SearchType.ALL):
+	var names = []
+	var dir = DirAccess.open(root_folder)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if (search_type == SearchType.ALL or search_type == SearchType.DIR) and dir.current_is_dir():
+				print("Found directory: " + file_name)
+				names.append(file_name)
+			elif (search_type == SearchType.ALL or search_type == SearchType.FILE) and not dir.current_is_dir():
+				print("Found file: " + file_name)
+				names.append(file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+	return names
---
+++ Components/audio_button.gd
@@ -0,0 +1,27 @@
+extends Button
+
+func load_sound(sound_file_path : String) -> void:
+	var sound_file = FileAccess.open(sound_file_path, FileAccess.READ)
+	var ext = sound_file_path.get_extension()
+	var stream : AudioStream
+	var data = sound_file.get_buffer(sound_file.get_length())
+	if ext == "mp3":
+		var s = AudioStreamMP3.new()
+		$Sound.stream = s
+	elif ext == "ogg":
+		## TODO Vorbis not working like this currently
+		var s = AudioStreamOggVorbis.new()
+		$Sound.stream = s
+	elif ext == "wav":
+		$Sound.stream = AudioStreamWAV.new()
+	sound_file.close()
+	$Sound.stream.data = data
+
+func load_icon(icon_file_path : String) -> void:
+	var image = Image.load_from_file(icon_file_path)
+	if image != null:
+		var texture = ImageTexture.create_from_image(image)
+		self.icon = texture
+
+func _on_pressed() -> void:
+	$Sound.play()
