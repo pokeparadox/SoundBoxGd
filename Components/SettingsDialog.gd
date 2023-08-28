dbd34e0721dfcf18c08f03413f8657dbf0ee9b97
PokeParadox <pokeparadox@gmail.com>
2023-08-28 11:40:18 +0100

    Added licensing and credits

--- Components/SettingsDialog.gd
+++ Components/SettingsDialog.gd
@@ -2,11 +2,11 @@ extends VBoxContainer
 
 func _ready() -> void:
 	if SettingsManager.settings_loaded():
-		$LineEditTwitchChannel.text = SettingsManager.settings.twitch_channel
+		$TwitchChannel/LineEditTwitchChannel.text = SettingsManager.settings.twitch_channel
 		$CheckBoxAutoLogin.button_pressed = SettingsManager.settings.auto_login
 
 func _on_button_save_pressed() -> void:
-	SettingsManager.settings.twitch_channel = $LineEditTwitchChannel.text
+	SettingsManager.settings.twitch_channel = $TwitchChannel/LineEditTwitchChannel.text
 	SettingsManager.settings.auto_login = $CheckBoxAutoLogin.button_pressed
 	SettingsManager.save_settings()
 	return_to_main()
