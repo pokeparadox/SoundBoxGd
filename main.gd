fa5710b2280ea610dfab339bcf627df2f2260f41
PokeParadox <pokeparadox@gmail.com>
2023-10-18 20:56:16 +0100

    Update main.tscn

--- main.gd
+++ main.gd
@@ -4,19 +4,21 @@ const TWITCH_CLIENT_ID : String = "0cbzay6zw9wmy56di4qd1tvmjkmh0w"
 var _twitch_secret: String
 
 func _init() -> void:
+	pass
 	var key = CryptoKey.new()
 	var crypto = Crypto.new()
-	key.load("res://generated.key")
+	key.load("res://generated.res")
 	var file = FileAccess.open("res://encrypted.res", FileAccess.READ)
 	if file != null:
 		_twitch_secret = crypto.decrypt(key, file.get_buffer(file.get_length())).get_string_from_utf8()
 	
 
 func _ready() -> void:
+	pass
 	if SettingsManager.settings_loaded():
 		if SettingsManager.settings.auto_login and !$Gift.connected:
 			await($Gift.authenticate(TWITCH_CLIENT_ID, _twitch_secret))
-			var success = await($Gift.connect_to_irc())
+			var _success = await($Gift.connect_to_irc())
 			#if (success):
 				#$Gift.request_caps()
 				#$Gift.join_channel(SettingsManager.settings.twitch_channel)
