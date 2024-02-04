58b5fdcb046d55082a572f623fcd242832a499b8
PokeParadox <pokeparadox@gmail.com>
2024-02-04 20:31:10 +0000

    Stabilised recording on android

--- ButtonMicrophone.gd
+++ ButtonMicrophone.gd
@@ -6,15 +6,20 @@ var recording
 func _ready() -> void:
 	var idx = AudioServer.get_bus_index("Record")
 	effect = AudioServer.get_bus_effect(idx, 0)
-
+	
 func _on_button_down() -> void:
-	print("Started Recording...")
-	effect.set_recording_active(true)
+	if $AudioStreamPlayer.playing:
+		$AudioStreamPlayer.stop()
+		recording = null
+	if not $AudioStreamRecord.playing:
+		$AudioStreamRecord.play()
+		effect.set_recording_active(true)
 
 
 func _on_button_up() -> void:
 	recording = effect.get_recording()
 	effect.set_recording_active(false)
-	print("Stopped Recording.")
-	$AudioStreamPlayer.stream = recording
-	$AudioStreamPlayer.play()
+	$AudioStreamRecord.stop()
+	if recording != null:
+		$AudioStreamPlayer.stream = recording
+		$AudioStreamPlayer.play()
